209954904d19c02b7fffcd1b4c5bc221ce816932
Rishi Vishwakarma <rishi24x7@rocketmail.com>
2016-02-22 16:02:09 +0530

    Jinput updated

--- addUsers.php
+++ addUsers.php
@@ -1,3 +1,4 @@
+<?php
 define('_JEXEC', 1);
 define('JPATH_BASE', __DIR__);
 define('DS', DIRECTORY_SEPARATOR);
@@ -16,27 +17,32 @@ jimport('joomla.mail.helper');
 jimport('joomla.user.helper');
 $language = JFactory::getLanguage();
 $language->load('com_users', JPATH_SITE);
+
+$jinput = JFactory::getApplication()->input;
+
 $type       = 0;
-$username   = JInput::getVar('username');
-$password   = JInput::getVar('password');
-$name       = JInput::getVar('name');
-$mobile     = JInput::getVar('mobile');
-$email      = JInput::getVar('email');
+$name       = $jinput->get('name', '', 'string');
+$username   = $jinput->get('username', '', 'string');
+$email      = $jinput->get('email', '', 'email');
+$password   = $jinput->get('password', '', 'string');
 $alias      = strtr($name, array(' ' => '-'));
 $sendEmail  = 1;
 $activation = 0;
 
-$data       = array('username'   => $username,
-            'name'       => $name,
-            'email1'     => $email,
-            'password1'  => $password, // First password field
-            'password2'  => $password, // Confirm password field
-            'sendEmail'  => $sendEmail,
-            'activation' => $activation,
-            'block'      => "0", 
-            'mobile'     => $mobile,
-            'groups'     => array("2", "10"));
-$response   = $model->register($data);
+$data = array(
+    'username'   => $username,
+    'name'       => $name,
+    'email1'     => $email,
+    'password1'  => $password, // First password field
+    'password2'  => $password, // Confirm password field
+    'sendEmail'  => $sendEmail,
+    'activation' => $activation,
+    'block'      => "0", 
+    'groups'     => array("2", "10")
+);
 
-echo $data['name'] . " saved!";
+$response = $model->register($data);
+
+//print_r($data); //tested
 $model->register($data);
+?>
